91bb19e8443f17ca980d522a542d7d35f73c7efe
Francisco Hugo Lezik <franciscohugolezik@gmail.com>
2024-09-14 10:32:58 -0300

    Commit 34

--- src/main.py
+++ src/main.py
@@ -3,43 +3,53 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/cantidad_mes")
 async def cantidad_filmaciones_mes():
-    print(10, 'peliculas fueron extrenadas en el mes de', 'Octubre')
+    answer = (10, 'peliculas fueron extrenadas en el mes de', 'Octubre')
+    print(answer)
+    return(answer)
 
 
-@app.get("/")
+@app.get("/cantidad_dia")
 async def cantidad_filmaciones_dia():
-    print(2, 'peliculas fueron estrenadas en los días', 'martes')
+    answer = (2, 'peliculas fueron estrenadas en los días', 'martes')
+    print(answer)
+    return(answer)
 
 
-@app.get("/")
+@app.get("/score_titulo")
 async def score_titulo():
-    print('La película', 'Star Wars, La Venganza de los Sith', 'fue estrenada en el año', \
-        2005, 'con un score/popularidad de', 85.5)
+    answer = ('La película', 'Star Wars, La Venganza de los Sith', 'fue estrenada en el año', \
+             2005, 'con un score/popularidad de', 85.5)
+    print(answer)
+    return(answer)
 
-
-@app.get("/")
+@app.get("/votos_titulo")
 async def votos_titulo():
-    print('La película', 'Star Wars, La Venganza de los Sith', 
-            'fue estrenada en el año', 2005, '.', 'La misma cuenta con un total de', 
-            1000, 'valoraciones, con un promedio de', 9.3)
+    answer = ('La película', 'Star Wars, La Venganza de los Sith', 
+             'fue estrenada en el año', 2005, '.', 'La misma cuenta con un total de', 
+             1000, 'valoraciones, con un promedio de', 9.3)
+    print(answer)
+    return(answer)
 
 
-@app.get("/")
+@app.get("/actor")
 async def get_actor():
-    print('El actor', 'Pedro', 'ha participado en', 20, 
-            'películas. Logró un retorno de', 20000000, 
-            'de dolares con un promedio de', 1000000, 'de dolares por película.')
+    answer = ('El actor', 'Pedro', 'ha participado en', 20, 
+             'películas. Logró un retorno de', 20000000, 
+             'de dolares con un promedio de', 1000000, 'de dolares por película.')
+    print(answer)
+    return(answer)
 
-
-@app.get("/")
+@app.get("/director")
 async def get_director():
-    print('El director', 'Juan', 'que ha obtenido un exito que medido en dolares es de', 
-            12000000, 'de dolares. Las películas que dirigio son:','\n',
-            'La Pelicula', 'estrenada en el año', 2000, 'con un retorno de', 
-            40000000, 'un costo de', 1000000, 'y una ganancia de', 30000000,'.','\n',
-            'La Pelicula 2', 'estrenada en el año', 2003, 'con un retorno de', 
-            70000000, 'un costo de', 2000000, 'y una ganancia de', 40000000,'.','\n',
-            'La Pelicula 3', 'estrenada en el año', 2007, 'con un retorno de', 
-            80000000, 'un costo de', 3000000, 'y una ganancia de', 50000000,'.')
+    answer = ('El director', 'Juan', 'que ha obtenido un exito que medido en dolares es de', 
+             12000000, 'de dolares. Las películas que dirigio son:','\n',
+             'La Pelicula', 'estrenada en el año', 2000, 'con un retorno de', 
+             40000000, 'un costo de', 1000000, 'y una ganancia de', 30000000,'.','\n',
+             'La Pelicula 2', 'estrenada en el año', 2003, 'con un retorno de', 
+             70000000, 'un costo de', 2000000, 'y una ganancia de', 40000000,'.','\n',
+             'La Pelicula 3', 'estrenada en el año', 2007, 'con un retorno de', 
+             80000000, 'un costo de', 3000000, 'y una ganancia de', 50000000,'.')
+    print(answer)
+    return(answer)
