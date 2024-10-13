e87c8253f1b94420646751b61d738cc805067d82
Francisco Hugo Lezik <franciscohugolezik@gmail.com>
2024-10-13 17:54:01 -0300

    branch_1 Commit 47

--- src/main.py
+++ src/main.py
@@ -19,6 +19,10 @@ routers = (
     actor.router, 
     director.router
 )
-
 for router in routers:
     app.include_router(router)
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
